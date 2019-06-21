d02a50069de70ea247fc39512aec42e695d12ecf
MinexAutomation <minex.automation@gmail.com>
2019-06-20 21:35:49 -0400

    Fix to make NupkgFilePath actually derive from FilePath.

--- source/R5T.Tools.NuGet.Types/Code/IO/Classes/NupkgFilePath.cs
+++ source/R5T.Tools.NuGet.Types/Code/IO/Classes/NupkgFilePath.cs
@@ -5,7 +5,7 @@ using R5T.NetStandard.IO.Paths;
 
 namespace R5T.Tools.NuGet.IO
 {
-    public class NupkgFilePath : FileExtension
+    public class NupkgFilePath : FilePath
     {
         public NupkgFilePath(string value)
             : base(value)
