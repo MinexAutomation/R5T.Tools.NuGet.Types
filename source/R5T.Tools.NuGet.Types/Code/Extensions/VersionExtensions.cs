01013246d30c23cd1f36e724fb320b2b02b122d3
MinexAutomation <minex.automation@gmail.com>
2019-07-05 15:12:58 -0400

    Added Version ToString() extension method to standardize version string for display.

---
+++ source/R5T.Tools.NuGet.Types/Code/Extensions/VersionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace R5T.Tools.NuGet
+{
+    public static class VersionExtensions
+    {
+        public static string ToStringDisplay(this Version version)
+        {
+            var output = version.ToString();
+            return output;
+        }
+    }
+}
